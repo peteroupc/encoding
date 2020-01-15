479127e1e3add9ae89c72bbae6f821f96a141530
Peter O <poccil14@gmail.com>
2020-01-15 00:53:26 -0500

    version 0.6.0

--- Encoding20/Properties/AssemblyInfo.cs
+++ Encoding20/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 [assembly: System.CLSCompliant(true)]
-[assembly: AssemblyInformationalVersion("0.5.1")]
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyInformationalVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
 [assembly: AssemblyProduct("Encoding Library")]
 [assembly: AssemblyTitle("Encoding Library")]
 [assembly: AssemblyDescription("A portable library for .NET that impleme" +
